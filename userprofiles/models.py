8f8d952079ad5b0b76b97621d3599a8ecd1075af
Rudy25 <rsmedina64@gmail.com>
2014-11-02 08:52:08 -0500

    first commit

---
+++ userprofiles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import Use
+
+class UserProfile(models.Model):
+
+avatar = models.ImageField(upload_to='avatars') #Pil ---> pillow
+    user = models.OneToOneField(
+
+    def __unicode__(selft):
+        return selft.user.username
+
+class UserTrack(models.Model):
+    cont = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User)
+
+
+# Create your models here.
