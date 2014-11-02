0e836b740f4151f0fb944a01a3350208e66a0c22
Rudy25 <rsmedina64@gmail.com>
2014-11-02 08:59:31 -0500

    first commitaa

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import Use
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-
-avatar = models.ImageField(upload_to='avatars') #Pil ---> pillow
-    user = models.OneToOneField(
+    avatar = models.ImageField(upload_to='avatars') #Pil ---> pillow
+    user = models.OneToOneField(User)
 
     def __unicode__(selft):
         return selft.user.username
